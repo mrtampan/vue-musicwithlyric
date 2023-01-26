63fd759822b617236fd788be024d23ace1fd60fb
achmadrivaldi <mrtampan54@gmail.com>
2023-01-26 19:39:29 +0700

    first commit

---
+++ src/views/BerandaView.vue
@@ -0,0 +1,13 @@
+<script setup>
+import AudioPlayer from "../components/AudioPlayer.vue";
+</script>
+
+<template>
+  <main>
+    <AudioPlayer />
+  </main>
+</template>
+
+<script>
+export default {};
+</script>
